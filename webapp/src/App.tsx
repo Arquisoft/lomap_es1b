26e35641c0e7bcf45f30f1dfb1f0f78e89cf81ab
jhergig <jhergig@gmail.com>
2023-03-01 02:14:00 +0100

    componente login inicial

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,29 +5,36 @@ import Container from '@mui/material/Container';
 import EmailForm from './components/EmailForm';
 import Welcome from './components/Welcome';
 import UserList from './components/UserList';
-import  {getUsers} from './api/api';
-import {User} from './shared/shareddtypes';
+import LoginForm from './components/LoginForm';
+import { getUsers } from './api/api';
+import { User } from './shared/shareddtypes';
 import './App.css';
 
 function App(): JSX.Element {
 
-  const [users,setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<string>("");
 
   const refreshUserList = async () => {
     setUsers(await getUsers());
   }
 
-  useEffect(()=>{
+  const refreshUserName = async (name:string) => {
+    setUser(name)
+  }
+
+  useEffect(() => {
     refreshUserList();
-  },[]);
+  }, []);
 
   return (
     <>
       <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
-        <UserList users={users}/>
+        <Welcome message={user} />
+        <Box component="div" sx={{ py: 2 }}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
+        <EmailForm OnUserListChange={refreshUserList} />
+        <UserList users={users} />
+        <LoginForm OnUserIsLoggedChange={refreshUserName} />
         <Link href="https://github.com/arquisoft/lomap_0">Source code</Link>
       </Container>
     </>
