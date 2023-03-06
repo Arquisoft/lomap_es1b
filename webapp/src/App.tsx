faa39acac9026ffa647b5c01178eeac5a2a85215
Pablo <uo265314@uniovi.es>
2023-03-06 23:42:16 +0100

    Inicio de funcionalidad para añadir amigos.
    Mejorado tipo User con id uuid v4 y lista de amigos.
    
    Añadida funcionalidad básica para añadir amigos a usuarios desde GUI.
    
    Añadido GET en la api para obtener la lista de amigos.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -9,6 +9,8 @@ import  {getUsers} from './api/api';
 import {User} from './shared/shareddtypes';
 import './App.css';
 
+import FriendForm from './components/FriendForm';
+
 function App(): JSX.Element {
 
   const [users,setUsers] = useState<User[]>([]);
@@ -21,13 +23,22 @@ function App(): JSX.Element {
     refreshUserList();
   },[]);
 
+
+
+
+
   return (
     <>
       <Container maxWidth="sm">
         <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
+        <Box component="div" sx={{ py: 2}}>
+          This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.
+        </Box>
         <EmailForm OnUserListChange={refreshUserList}/>        
         <UserList users={users}/>
+        
+        <FriendForm /> // Añadimos el componente FriendForm
+        
         <Link href="https://github.com/arquisoft/lomap_0">Source code</Link>
       </Container>
     </>
