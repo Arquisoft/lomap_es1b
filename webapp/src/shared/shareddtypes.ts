36de437b6d56e2d31f72bb3d254b4ba50ff690b9
UO284541 <oscardavilasampedro@gmail.com>
2023-03-12 16:59:34 +0100

    Eliminado código redundante, conflicto solucionado

--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,6 +1,15 @@
 export type User = {
-    id: string;
-    name:string;
-    email:string;
-    friends: Array<string>;
-  }
+  id: string;
+  name: string;
+  email: string;
+  friends: Array<string>;
+}
+
+export interface IPMarker {
+  id: number
+  date: Date
+  lat: number
+  lng: number
+  name: string
+  description: string
+}
