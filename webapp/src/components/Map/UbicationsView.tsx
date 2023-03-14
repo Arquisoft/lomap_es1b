d3df0a34f2aaff7fc3b7ef5fd957f6223f22ca51
Jhergig <jhergig@gmail.com>
2023-03-14 19:35:29 +0100

    vista Mis ubicaciones con datos del pod

--- webapp/src/components/Map/UbicationsView.tsx
+++ webapp/src/components/Map/UbicationsView.tsx
@@ -1,20 +1,20 @@
 import { Button, Grid, Box } from '@mui/material';
 import { IPMarker } from "../../shared/shareddtypes";
+import { MarkerContext } from '../../context/MarkerContextProvider';
+import { useState, useEffect, useContext } from 'react';
 
 const UbicationsView = () => {
 
-    var ubications:IPMarker[] = [{ id: 1, date: new Date(), name: "Ubicacion1", description: "Ubicacion1 Desc", lat: 43, lng: -3 }, { id: 2, date: new Date(), name: "Ubicacion2", description: "Ubicacion2 Desc", lat: 44, lng: -3 },
-    { id: 3, date: new Date(), name: "Ubicacion3", description: "Ubicacion3 Desc", lat: 42, lng: -3 }, { id: 4, date: new Date(), name: "Ubicacion4", description: "Ubicacion4 Desc", lat: 43, lng: -2 },
-    { id: 5, date: new Date(), name: "Ubicacion5", description: "Ubicacion5 Desc", lat: 44, lng: -2 }, { id: 6, date: new Date(), name: "Ubicacion6", description: "Ubicacion6 Desc", lat: 42, lng: -2 }];
+    const { state: markers, dispatch } = useContext(MarkerContext);
 
     return (
         <>
             {
-                ubications.length > 0
+                markers.length > 0
                     ? (
                         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ padding: '2em' }}>
                             {
-                                ubications.map((ubication: IPMarker) => {
+                                markers.map((ubication: IPMarker) => 
                                     <Grid item xs={6} sm={4} md={3}>
                                         <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
                                             <h1>{ubication.name}</h1>
@@ -24,71 +24,15 @@ const UbicationsView = () => {
                                             <Button>Borrar</Button>
                                         </Box>
                                     </Grid>
-                                })
+                                )
                             }
                         </Grid>
                     ) : (
                         <p>Aún no has creado ninguna ubicación</p>
                     )
             }
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ padding: '2em' }}>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
-                        <h1>Ubicación 1</h1>
-                        <p>Ubicación 1</p>
-                        <p>43,93206919289874</p>
-                        <p>-7,657041674999993</p>
-                        <Button>Borrar</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
-                        <h1>Ubicación 2</h1>
-                        <p>Ubicación 2</p>
-                        <p>43,93206919289874</p>
-                        <p>-7,657041674999993</p>
-                        <Button>Borrar</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
-                        <h1>Ubicación 3</h1>
-                        <p>Ubicación 3</p>
-                        <p>43,93206919289874</p>
-                        <p>-7,657041674999993</p>
-                        <Button>Borrar</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
-                        <h1>Ubicación 4</h1>
-                        <p>Ubicación 4</p>
-                        <p>43,93206919289874</p>
-                        <p>-7,657041674999993</p>
-                        <Button>Borrar</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
-                        <h1>Ubicación 5</h1>
-                        <p>Ubicación 5</p>
-                        <p>43,93206919289874</p>
-                        <p>-7,657041674999993</p>
-                        <Button>Borrar</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
-                        <h1>Ubicación 6</h1>
-                        <p>Ubicación 6</p>
-                        <p>43,93206919289874</p>
-                        <p>-7,657041674999993</p>
-                        <Button>Borrar</Button>
-                    </Box>
-                </Grid>
-            </Grid>
         </>
-    );
+    ); 
 }
 
 export default UbicationsView;
