cfdc82e89052e286ef6dbde86f01890b199bac2a
UO284541 <oscardavilasampedro@gmail.com>
2023-04-02 18:27:41 +0200

    Añadida opción "Otro" al login.

--- webapp/src/components/login/LoginForm.tsx
+++ webapp/src/components/login/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { LoginButton } from "@inrupt/solid-ui-react";
-import { Button, Dialog, FormGroup, Stack, Select, MenuItem, SelectChangeEvent } from "@mui/material";
+import { Button, Dialog, FormGroup, Stack, Select, MenuItem, SelectChangeEvent, TextField, Container } from "@mui/material";
 
 export interface LoginProps {
   open: boolean;
@@ -8,35 +8,58 @@ export interface LoginProps {
 }
 
 function LoginForm(props: LoginProps): JSX.Element {
-  const [oidcIssuer, setOidcIssuer] = useState<string>("");
   const { onClose, open } = props;
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const [oidcIssuer, setOidcIssuer] = useState<string>("");
+  const [itemSelected, setItemSelected] = useState<string>("");
+  
   const handleClose = () => {
     onClose();
   };
 
-  const seleccionarProveedor = (e: SelectChangeEvent) => {
-    setOidcIssuer(e.target.value as string);
-  };
+  useEffect(() => {
+    if (itemSelected != "otro") {
+      setOidcIssuer(itemSelected);
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [itemSelected])
 
   return (
     <Dialog onClose={handleClose} open={open}>
-      <Stack direction={{ xs: 'column', sm: 'row' }} alignItems='center' sx={{ flexGrow: '2' }} justifyContent='flex-end' spacing={{ xs: 1, sm: 2, md: 4 }}>
-        <FormGroup>
-          <Select
-            value={oidcIssuer}
-            onChange={seleccionarProveedor}
-            sx={{ width: '15em' }}
-          >
-            <MenuItem value={"https://inrupt.net/"}>Inrupt</MenuItem>
-            <MenuItem value={"https://solidcommunity.net/"}>SOLID community</MenuItem>
-          </Select>
-          <LoginButton oidcIssuer={oidcIssuer} redirectUrl="http://localhost:3000">
-            <Button variant="contained" >
-              Iniciar sesión
-            </Button>
-          </LoginButton>
-        </FormGroup>
-      </Stack>
+      <Container sx={{ display: 'flex', padding: '0.5em !important' }}>
+        <div style={{ margin: '1em' }}>
+          <p>Por favor, seleccione un proveedor.</p>
+        </div>
+        <div>
+          <Stack direction={{ xs: 'column', sm: 'row' }} alignItems='center' sx={{ flexGrow: '2' }} justifyContent='flex-end' spacing={{ xs: 1, sm: 2, md: 4 }}>
+            <FormGroup>
+              <Select
+                value={itemSelected}
+                onChange={e => setItemSelected(e.target.value as string)}
+                sx={{ width: '15em', margin: '0.5em' }}
+              >
+                <MenuItem value={"https://inrupt.net/"}>Inrupt</MenuItem>
+                <MenuItem value={"https://solidcommunity.net/"}>SOLID community</MenuItem>
+                <MenuItem value={"otro"}>Otro proveedor</MenuItem>
+              </Select>
+              <TextField
+                type="url"
+                value={oidcIssuer}
+                onChange={e => setOidcIssuer(e.target.value as string)}
+                sx={{ width: '15em', margin: '0.5em' }}
+                disabled={disabled}
+              />
+              <LoginButton oidcIssuer={oidcIssuer} redirectUrl="http://localhost:3000">
+                <Button variant="contained" sx={{ width: '17.25em', margin: '0.5em' }}>
+                  Iniciar sesión
+                </Button>
+              </LoginButton>
+            </FormGroup>
+          </Stack>
+        </div>
+      </Container>
     </Dialog>
   );
 }
